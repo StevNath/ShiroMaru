db5b52de009af4a47931f8280bc9b2354df5d602
StevNath <stevennmeliala@gmail.com>
2026-03-05 19:06:37 +0700

    Create upload.js

---
+++ commands/utility/upload.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('uploadjadwal')
+		.setDescription('Uploads the schedule file to the server')
+        .addAttachmentOption(option =>
+            option.setName('file')
+                .setDescription('The schedule file to upload')
+                .setRequired(true)
+        ),
+	async execute(interaction) {
+        const allowedRole = "Ordal"
+
+        if (!interaction.member.roles.cache.some(role => role.name === allowedRole)) {
+            return interaction.reply({
+                content: 'Kamu tidak punya izin untuk menggunakan command ini!',
+                ephemeral: true
+            });
+        }
+
+		const attachment = interaction.options.getAttachment('file');
+
+        const cacheDir = path.join(__dirname, '../../cache');
+        if (!fs.existsSync(cacheDir)) {
+            fs.mkdirSync(cacheDir);
+        }
+
+        const filePath = path.join(cacheDir, attachment.name);
+        const file = fs.createWriteStream(filepath);
+
+        https.get(attachment.url, (response) => {
+            response.pipe(file);
+            file.on('finish', () => {
+                file.close();
+                interaction.reply({ content: 'File uploaded successfully!', ephemeral: true });
+            });
+        }).on('error', (err) => {
+            fs.unlink(filePath, () => {});
+            console.error(err);
+            interaction.reply({ content: 'Failed to upload file.', ephemeral: true });
+        });
+	},
+}
